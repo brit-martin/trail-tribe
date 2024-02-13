4447171ccd9842d5bf1020945515b5a754d54331
Brittany Larson <brittanylarson@Brittanys-Air.localdomain>
2024-02-13 14:04:41 -0700

    create contoller folder with controller files

---
+++ server/controllers/authCtrl.js
@@ -0,0 +1,14 @@
+export default {
+    signUp: async (req, res) => {
+        console.log('signup router')
+    },
+    login: async (req, res) => {
+        console.log('login router')
+    },
+    logout: async (req, res) => {
+        console.log('logout router')
+    },
+    editUserInfo: async (req, res) => {
+        console.log('user edit router')
+    }
+}
---
+++ server/controllers/notificationCtrl.js
@@ -0,0 +1,5 @@
+export default {
+    notification: async (req, res) => {
+        console.log('notification router')
+    }
+}
