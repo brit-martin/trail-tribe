ba8510798b76eccd6bad68c7ce074f81178a72bc
Brittany Larson <brittanylarson@Brittanys-Air.localdomain>
2024-02-15 15:12:42 -0700

    working on functionality

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,108 +1,156 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import IconButton from '@mui/material/IconButton';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import InputAdornment from '@mui/material/InputAdornment';
-import Button from '@mui/material/Button';
-import '../styles/signup.css'
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import IconButton from "@mui/material/IconButton";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import InputAdornment from "@mui/material/InputAdornment";
+import Button from "@mui/material/Button";
+import "../styles/signup.css";
+import { useState } from "react";
+import axios from 'axios';
 
 function Signup() {
+  let userBody = {
+    fname: fnameInput,
+    lname: lnameInput,
+    email: emailInput,
+    password: passwordInput,
+  };
+  axios
+  .post('/signup', userBody)
+  .then((response) => {
+    console.log(response.data)
+  })
+
+
   const [showPassword, setShowPassword] = React.useState(false);
+  const [showConfirmPassword, setConfirmShowPassword] = React.useState(false);
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleClickShowConfirmPassword = () => setConfirmShowPassword((show) => !show);
 
-  // const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-  //   event.preventDefault();
+  const [fnameInput, setFnameInput] = useState('')
+  const [lnameInput, setLnameInput] = useState('')
+  const [emailInput, setEmailInput] = useState('')
+  const [passwordInput, setPasswordInput] = useState('')
+  const [confirmPasswordInput, setConfirmPasswordInput] = useState('')
 
   return (
-    
-    <div className='signup-page'>
+    <div className="signup-page">
+      <div className="signup-wrapper"></div>
       <Box
+        className="signup-form"
         component="form"
-        sx={{'& .MuiTextField-root' : {m: 1, width: '25ch'}
-      }}
+        sx={{ "& .MuiTextField-root": { m: 1, width: "25ch" } }}
         noValidate
-        autoComplete='off'
+        autoComplete="off"
       >
-        <div className='signup-form'>
-          <TextField
+        <div className="signup-form">
+          <h1 className="signup-header">Sign Up</h1>
+          <h3 className="signup-header">Join the tribe today!</h3>
+          <TextField 
+            className='singup-input' 
             required
-            id='outlined-required'
-            label="First Name"
-          />
-          <TextField
-            required
-            id='outlined-required'
-            label='Last Name'
-          />
-          <TextField
-            required
-            id='outlined-required'
-            label='Email'
-          />
-          <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined" required>
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            // type='password'
-            type={showPassword ? 'text' : 'password'}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  // onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
-          /> 
-        </FormControl>
-        <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined" required>
-          <InputLabel htmlFor="outlined-adornment-password">Confirm Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            // type='password'
-            type={showPassword ? 'text' : 'password'}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  // onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
-          />
+            id="outlined-required" 
+            label="First Name" 
+            value={fnameInput}
+            onChange={(e) => setFnameInput(e.target.value)}
+            />
+          <TextField 
+            className='singup-input'
+            required 
+            id="outlined-required" 
+            label="Last Name" 
+            value={lnameInput}
+            onChange={(e) => setLnameInput(e.target.value)}
+            />
+          <TextField 
+            className='singup-input'
+            required 
+            id="outlined-required" 
+            label="Email" 
+            value={emailInput}
+            onChange={(e) => setEmailInput(e.target.value)}
+            />
+          <FormControl 
+            className='singup-input' 
+            sx={{ m: 1, width: "25ch" }} 
+            variant="outlined" 
+            required 
+            value={confirmPasswordInput}
+            onChange={(e) => setPasswordInput(e.target.value)}
+            >
+            <InputLabel htmlFor="outlined-adornment-password">
+              Password
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              // type='password'
+              type={showPassword ? "text" : "password"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    // onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
           </FormControl>
-        </div>
-        <Button variant="contained">Sign Up</Button>
-        <span>
-              Already a member of the tribe?{" "}
-              <a
-                href="#"
-                onClick={() => {
-                  setShowLogin(true);
-                }}
-              >
-                Sign in?
-              </a>
-            </span>
+          <FormControl 
+            className='singup-input' 
+            sx={{ m: 1, width: "25ch" }} 
+            variant="outlined" 
+            required 
+            value={confirmPasswordInput}
+            onChange={(e) => setConfirmPasswordInput(e.target.value)}
+            >
+            <InputLabel htmlFor="outlined-adornment-password">
+              Confirm Password
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              // type='password'
+              type={showConfirmPassword ? "text" : "password"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowConfirmPassword}
+                    // onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+        <Button className="signup-button" variant="contained">Sign Up</Button>
+        <span className="signup-footer">
+          Already a member of the tribe?{" "}
+          <a
+            href="#"
+            onClick={() => {
+              setShowLogin(true);
+            }}
+            >
+            Sign in?
+          </a>
+        </span>
+      </div>
       </Box>
     </div>
-    
   );
 }
 
