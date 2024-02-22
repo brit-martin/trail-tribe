5316e592abdfb9947b99307e380b5a60bcef6e2d
Jake Nichols <yummyfunnybunny13@gmail.com>
2024-02-22 15:03:43 -0700

    remove all unused code and console.logs. add comments to explore.jsx and mapbox.jsx

--- src/components/Mapbox.jsx
+++ src/components/Mapbox.jsx
@@ -4,40 +4,23 @@ import '../styles/mapbox.css';
 // packages
 import { useEffect, useRef, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
-import { useSelector, useDispatch } from 'react-redux';
 
 mapboxgl.accessToken = import.meta.env.VITE_REACT_APP_MAPBOXTOKEN;
 
+// TODO
+// IMPORTANT - WHEN YOU MAKE A SAVE TO THIS FILE, ALL OF THE OLD MARKERS GET DELETED.
+// THIS MEANS THAT WE JUST NEEDD TO CHANGE THE WAY THE MAPBOX RE-RENDERS TO FIX THE ISSUE
+// OF OLD MARKERS NOT BEING DELETED
+
 function Mapbox(props) {
-  const dispatch = useDispatch();
-  const reduxLocationData = useSelector((state) => state.locationData);
-  // const reduxMarkers = useSelector((state) => state.currentMarkersReducer.currentMarkers);
-  // console.log(reduxMarkers);
-  // const [currentMarkers, setCurrentMarkers] = useState([]);
+  // INITS
   const map = useRef(null);
-  // const [map, setMap] = useState();
   const currentMarkers = useRef([]);
-  console.log(currentMarkers);
-
-  // console.log(props.locationData);
-  //stores the dom element in the useRef
-  const [locationData, setLocationData] = useState(props.locationData);
   const mapContainer = useRef(null);
-  // const currentMarkers = [];
-  // const [lng, setLng] = useState(-111.8746681);
-  // const [lat, setLat] = useState(40.4194344);
   const [zoom, setZoom] = useState(12);
-  // const { setBoundingBox } = props;
-  // console.log('locationData:');
-  // console.log(props.locationData);
-  // console.log(locationData);
-  // console.log('currentMarkers:');
-  // console.log(currentMarkers);
 
-  // UseEffect
   useEffect(() => {
-    console.log('== Mapbox UseEffect ==');
-    // console.log('made it to useEffect!');
+    // Create the map object if it doesn't already exist
     if (!map.current) {
       map.current = new mapboxgl.Map({
         container: mapContainer.current,
@@ -46,43 +29,19 @@ function Mapbox(props) {
         zoom: zoom,
       });
     }
-    console.log(map);
-
-    map.current.on('load', () => {});
-
-    // console.log('made it past the return statement');
-
-    console.log('setting locationData');
-    setLocationData(props.locationData);
-
-    //step two: add markers to map---------------------------------------
-    // TODO - create the popup stuff
-
-    console.log('checking locationData.length');
 
     if (props.locationData.length > 0) {
-      console.log('locationData greater than 0');
-
-      // remove all current markers
-      console.log('checking currentMarkers.length');
-      // if (currentMarkers.current.length > 0) {
+      // Remove previous markers
       if (map.current._markers.length > 0) {
-        console.log(map.current._markers);
-        console.log('removing all currentMarkers');
-        map.current._markers.forEach((marker, idx) => {
-          console.log(marker);
-          // marker.remove();
-          // currentMarkers.current.shift(idx);
-          // setCurrentMarkers(currentMarkers[idx].remove());
+        map.current._markers.forEach((marker) => {
           marker.remove();
         });
       }
 
+      // Create the Markers
       props.locationData.forEach((marker) => {
+        // A: Set the HTML to place inside the popup
         const trailName = marker.tags.name ? marker.tags.name : 'None Provided';
-        // console.log(marker);
-
-        // 1: Set the HTML to place inside the popup
         const innerHtmlContent = `
           <div>
             <h2>Trail Details</h2>
@@ -92,32 +51,27 @@ function Mapbox(props) {
           </div>
         `;
 
-        // 2: create the HTMl container and the button
+        // B: create the HTMl container and the button
         const divElement = document.createElement('div');
         const assignBtn = document.createElement('div');
         assignBtn.innerHTML = `<button>Display Trail</button>`;
         divElement.innerHTML = innerHtmlContent;
         divElement.appendChild(assignBtn);
 
-        // 3: create the button event listener
+        // C: create the button to display the path
         assignBtn.addEventListener('click', (e) => {
-          console.log('== Clicked Marker ==');
-          // console.log(e.target);
-          // console.log(marker);
-          const trailPath = marker.geometry.map((step, idx) => {
-            // if (idx !== 0 && idx !== marker.geometry[marker.geometry.length - 1]) {
-            console.log(step);
-            return [step.lon, step.lat];
-            // }
-          });
-          trailPath.shift();
-          trailPath.pop();
-
+          //  i: remove any existing path before making a new one
           if (map.current.getLayer('line')) {
             map.current.removeLayer('line');
             map.current.removeSource('line');
           }
 
+          // ii: create the path array that holds all the path coords
+          const trailPath = marker.geometry.map((step) => {
+            return [step.lon, step.lat];
+          });
+
+          // iii: create the path
           map.current.addSource('line', {
             type: 'geojson',
             data: {
@@ -125,18 +79,12 @@ function Mapbox(props) {
               properties: {},
               geometry: {
                 type: 'LineString',
-                coordinates: [
-                  [marker.geometry[0].lon, marker.geometry[0].lat], // start coordinates [lng, lat]
-                  ...trailPath, // additional point
-                  [marker.geometry[marker.geometry.length - 1].lon, marker.geometry[marker.geometry.length - 1].lat], // end coordinates [lng, lat]
-                  // [-122.4293, 37.7699], // additional point
-                ],
+                coordinates: [...trailPath],
               },
             },
           });
 
-          console.log(map.current.getSource('line'));
-
+          // iv: create the path layer
           map.current.addLayer({
             id: 'line',
             type: 'line',
@@ -152,39 +100,25 @@ function Mapbox(props) {
           });
         });
 
-        // console.log(marker.geometry[0].lon);
-        // console.log(marker.geometry[0].lat);
-        // console.log(marker.lng);
-        // console.log(marker.lat);
-
-        // add new markers
-        console.log('creating new markers');
+        // D: add new markers
         const newMarker = new mapboxgl.Marker()
           .setLngLat([marker.geometry[0].lon, marker.geometry[0].lat])
           .setPopup(new mapboxgl.Popup().setDOMContent(divElement))
           .addTo(map.current);
+
         currentMarkers.current.push(newMarker);
-        // dispatch({ type: 'ADD_MARKER', payload: newMarker });
-        // setCurrentMarkers([...currentMarkers, newMarker]);
-        // console.log(currentMarkers);
       });
-      // console.log('currentMarkers:');
-      // console.log(currentMarkers);
     }
-    //step three: updater for long and lat and zoom states
-    // console.log('is there a map.current?');
+
+    // Updater long and lat and zoom states
     if (map.current) {
-      // console.log('map.current exists!');
       map.current.on('move', () => {
         props.setLng(map.current.getCenter().lng.toFixed(4));
         props.setLat(map.current.getCenter().lat.toFixed(4));
         setZoom(map.current.getZoom().toFixed(2));
-        // console.log('were fucking moving!!!!');
       });
     }
   }, [props.locationData]);
-
-  // Functions
 
   return (
     <>
