008287c45783c541ef29ed47476f0e7fac161132
Jake Nichols <yummyfunnybunny13@gmail.com>
2024-02-28 16:04:22 -0700

    added page-width to landing page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,65 +1,65 @@
-import React from "react";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import { useNavigate } from "react-router-dom";
-import { primHoverSX } from "./Theme.jsx";
-import { useTheme } from "@mui/material";
+import React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { useNavigate } from 'react-router-dom';
+import { primHoverSX } from './Theme.jsx';
+import { useTheme } from '@mui/material';
+import Container from '@mui/material/Container';
 
-import "../styles/Hero.css";
+import '../styles/Hero.css';
 
 function SingleCard() {
   const navigate = useNavigate();
   const theme = useTheme();
 
   const subheadingFont = {
-    fontFamily: theme.fontStyle.secondaryFont
-  }
+    fontFamily: theme.fontStyle.secondaryFont,
+  };
 
   function handleFindTribe() {
-    navigate("/signup");
+    navigate('/signup');
   }
-    
+
   return (
-    <>
-      <Card className="hero__card">
+    <Container className='page__width'>
+      <Card className='hero__card'>
         <CardContent>
-          <Typography variant="h5" component="h2" gutterBottom style={subheadingFont}>
+          <Typography variant='h5' component='h2' gutterBottom style={subheadingFont}>
             Step by Step, Friend by Friend
           </Typography>
-          <Typography variant="h4" component="h3" style={subheadingFont}>
+          <Typography variant='h4' component='h3' style={subheadingFont}>
             Step Into Adventure Together
           </Typography>
         </CardContent>
         <CardActions>
-          <Button 
-          size="small" 
-          variant="contained"
-          color='primary'
-          sx={primHoverSX}
-          onClick={handleFindTribe}
-          style={subheadingFont}
+          <Button
+            size='small'
+            variant='contained'
+            color='primary'
+            sx={primHoverSX}
+            onClick={handleFindTribe}
+            style={subheadingFont}
           >
             Find your Tribe
           </Button>
         </CardActions>
       </Card>
-    </>
+    </Container>
   );
 }
 
 export default function Hero() {
-  
   return (
-    <div className="hero__container">
-      <img className="hero__img" src="\random-mountain.jpg" alt="" />
+    <div className='hero__container page__container'>
+      <img className='hero__img' src='\random-mountain.jpg' alt='' />
 
-      <div className="hero__logo">
+      <div className='hero__logo'>
         <h2>
-          <span id="word__1">Trail</span> <span id="word__2">Tribe</span>
+          <span id='word__1'>Trail</span> <span id='word__2'>Tribe</span>
         </h2>
       </div>
       <SingleCard />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,17 +2,18 @@ import Benefits from './Benefits.jsx';
 import Hero from './Hero.jsx';
 import Testimonials from './Testimonials.jsx';
 import Features from './Features.jsx';
-
-
+import '../index.css';
 
 function Home() {
   return (
-    <>
-      <Hero />
-      <Features />
-      <Benefits />
-      <Testimonials/>
-    </>
+    <div className='page__bg'>
+      <div className='page__width'>
+        <Hero />
+        <Features />
+        <Benefits />
+        <Testimonials />
+      </div>
+    </div>
   );
 }
 
